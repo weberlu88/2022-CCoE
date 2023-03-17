a984cd46c6e3fd29da925e1ad5e5c1a42610f128
WeberLu <43847259+weberlu88@users.noreply.github.com>
2023-03-17 23:09:29 +0800

    可以跑 rule (但match到的動詞非常少)

---
+++ Model/TestZone/cc_nlp_script.py
@@ -0,0 +1,162 @@
+# import re
+# import os
+# from os import listdir
+# from os.path import isfile, join
+# from pathlib import Path
+# import pandas as pd
+from typing import Callable, Dict, List, get_type_hints
+import pandas as pd
+import spacy
+from spacy import displacy
+from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER, HYPHENS
+from spacy.lang.char_classes import CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
+from spacy.util import compile_infix_regex
+import os, psutil
+
+# https://stackoverflow.com/questions/66636097/prevent-spacy-tokenizer-from-splitting-on-specific-character
+def prepare_nlp() -> Callable:
+    nlp = spacy.load("en_core_web_sm")
+    text = "Get 10ct/liter off when using our App"
+    # Modify tokenizer infix patterns
+    infixes = (
+        LIST_ELLIPSES
+        + LIST_ICONS
+        + [
+            r"(?<=[0-9])[+\-\*^](?=[0-9-])",
+            r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
+                al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
+            ),
+            r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+            r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+            #r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+            r"(?<=[{a}0-9])[:<>=](?=[{a}])".format(a=ALPHA),
+        ]
+    )
+    infix_re = compile_infix_regex(infixes)
+    nlp.tokenizer.infix_finditer = infix_re.finditer
+
+    return nlp
+
+# a global funciton
+customized_nlp = prepare_nlp()
+
+def find_verb_of_vocab(sentence: str, target_word: str, lemma=True):
+    'Pass a sentence and a target_word, return the verb operates on the target_word or None. Lemmatization applied at default.'
+    doc = customized_nlp(sentence)
+    # find the target_word element (excat match)
+    target_elem = None
+    for i,token in enumerate(doc):
+        if token.text == target_word:
+            target_elem = token
+    if target_elem is None:
+        return None
+    # find the verb parent of the target_word element
+    token = target_elem
+    while token.head != token:
+        token = token.head
+        # verb found
+        if token.tag_.startswith('VB'):
+            if lemma:
+                return token.lemma_
+            return token.text
+    return None
+
+def syscall_verb_analysis(syscall: str, en_verb: str) -> tuple[bool, int]:
+    '''Pass a syscall and a verb, return they are similiar or not and the weight (2 or 1).
+    @return similiarity: boolean, weight: int'''
+    return 
+
+from enum import Enum
+
+class OperationMode(Enum):
+    FILE_READ_CLOSE = 0
+    FILE_CREATE_OPEN = 1
+    FILE_UPDATE = 2
+    FILE_DELETE = 3
+    PROCESS_EXE = 4
+    PROCESS_KILL = 5
+    PROCESS_Other = 6
+    DEVICE = 7
+    INFO_GET = 8
+    INFO_SET = 9
+    COMM_CONNECT = 10
+    COMM_TRANSMIT = 11
+
+class OperationEvaluator:
+    def __init__(self) -> None:
+        self.df = self.read_rule()
+        self.update_syscall_list = list(self.df[self.df['ActionType'].isin(['UPDATE'])]['Syscall'].unique())
+        pass
+
+    def read_rule(self) -> pd.DataFrame:
+        df = pd.read_csv('rule_dataset.csv')
+
+        def clean_braces(x):
+            x = x.replace("()", "").replace(",", "").replace(" ", "")
+            return str(x)
+
+        df['Syscall'] = df['Syscall'].apply(clean_braces)
+        return df
+
+    def translate_rule(self, entityType: str, enVerb: str, object: str=None) -> list[str] | None:
+        ''' translate a given enverb into system call(s) with hardcoded rule '''
+        entityType = entityType.upper()
+        if entityType.startswith('PROC'):
+            entityType = 'PROC'
+        if entityType.startswith('NET'):
+            entityType = 'NET'
+        if entityType.startswith('INFO'):
+            entityType = 'INFO'
+        entityType_lst = ['FILE', 'PROC', 'DEVICE', 'INFO', 'NET']
+        if entityType not in entityType_lst:
+            return None
+        
+        # Get rules belongs to this entityType
+        dataFileType = self.df[self.df['EntityType'] == entityType]
+        dataEnVerb = dataFileType.loc[dataFileType['EnVerb'].isin([enVerb])]
+        if len(dataEnVerb) == 0:
+            return None
+        return list(dataEnVerb['Syscall'].unique())
+
+    def resolve(self, entityType: str, syscall:str, en_verb:str, downcast=False) -> bool:
+        hit, isSysChange = False, False
+        corresponding_syscall_list = self.translate_rule(entityType, en_verb)
+        if corresponding_syscall_list and syscall in corresponding_syscall_list:
+            hit = True
+        elif entityType.upper() == "FILE" and syscall in self.update_syscall_list:
+            isSysChange = True
+        return hit, isSysChange
+
+if __name__ == '__main__':
+    # test 1, <connects, 23.224.59.34:48080>
+    sentence = 'the malware also connects to 23.224.59.34:48080 to send and receive remote shell commands'
+    target_word = '23.224.59.34:48080'
+    verb = find_verb_of_vocab(sentence, target_word)
+    print(f'test1: {verb}')
+
+    # test 2, <opened, /proc/cpuinfo>
+    sentence = 'the virtual file /proc/cpuinfo is opened and read in words'
+    target_word = '/proc/cpuinfo'
+    verb = find_verb_of_vocab(sentence, target_word)
+    print(f'test2: {verb}')
+
+    # OperationEvaluator test case
+    operationEvaluator = OperationEvaluator()
+    res = operationEvaluator.resolve('linkat', 'read')
+    print(res) # True [OperationMode.FILE_READ_CLOSE, OperationMode.FILE_READ_CLOSE]
+    res = operationEvaluator.resolve('linkat', 'connect')
+    print(res) # False
+    res = operationEvaluator.resolve('ioctl', 'send')
+    print(res) # [OperationMode.DEVICE, OperationMode.DEVICE]
+
+    mode = operationEvaluator.get_mode('linkat')
+    print("mode of 'linkat':", mode) # OperationMode.FILE_READ_CLOSE
+    mode = operationEvaluator.get_mode('read')
+    print("mode of 'read':", mode)   # OperationMode.FILE_READ_CLOSE
+    mode = operationEvaluator.get_mode('connect')
+    print("mode of 'connect':", mode) # OperationMode.COMM_TRANSMIT
+    mode = operationEvaluator.get_mode('install')
+    print("mode of 'install':", mode) # OperationMode.PROCESS_EXE
+    mode = operationEvaluator.get_mode('send')
+    print("mode of 'send':", mode) # OperationMode.COMM_CONNECT (其實也屬於DEVICE，但只能做到return一個mode)
+    
---
+++ Model/TestZone/cc_regex_script.py
@@ -0,0 +1,141 @@
+import re
+
+class RegexMatchResult:
+    def __init__(self, word: str, type: str=None, match_regex: str=None) -> None:
+        self.line = ''
+        self.word = word
+        self.type = type
+        self.match_regex = match_regex
+        pass
+    def __str__(self):
+        return f"<RegexMatchResult word={self.word}, type={self.type}, match_regex={self.match_regex}>"
+    def __repr__(self):
+        return f"<RegexMatchResult word={self.word}, type={self.type}, match_regex={self.match_regex}>"
+
+class RegexMaster:
+    def __init__(self, familyname:str=None) -> None:
+        self.used_regex_set = set() # 紀錄被 find_spacial_token() 找到過的 regex
+        self.regex_file = {
+            "sed command": "bin/sed", 
+            "startup": ["/etc/rc", "/etc/init.d/"], # 為AttacKG放寬標準的字串 , "rc.local", "init.d"
+            "process_info": "/proc/", 
+            "sed temp file": "/etc/sed", 
+            "selinx": "/selinux", 
+            "sys": ["/sys/", "bin/", "lsb-release"], 
+            "uname":"uname(.)($)",
+            "dns": ["mtab", "nsswitch.conf", "resolv.conf", "/hosts"],
+        }
+        self.regex_process = {"command": ["^sh$", "^sed$"]} # must exact match
+        self.regex_net = {
+            "net address":[r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+", r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"], # 把 port 砍了 r":\d+" 會抓到時間，如 11:48
+            "NIC": "eth[0-9]$"}
+        self.regex_mem = {"Memory Address": "0x[0-9a-zA-Z]{8}"}
+        self.regex_other = {"permission":"permission:{0,1}[0-9]{0,4}", "ID":["UID", "GID"]}
+        self.all_regex_dict = {**self.regex_file,  **self.regex_process, **self.regex_net, **self.regex_mem, **self.regex_other}
+        # 切換其他家族
+        if familyname and familyname.lower() == 'xorddos':
+            self.regex_file = {
+                "sed command": "bin/sed", 
+                "startup": ["/etc/rc", "/etc/init.d/"],
+                "proc info":"/proc/", 
+                "sed temp file":"/etc/sed", 
+                "selinux":"/selinux",
+                "boot":"/boot/", 
+                "rootkit component":"/proc/rs_dev",
+                "execution file created by xorddos":"/boot/[a-z]{10}", 
+                "run":["/run/", "/var/run/"],
+                "var": "/var/", 
+                "perl": "/perl/", 
+                "crontab": "/etc/cron",
+                "init process": "bin/openrc", 
+                "init service": "bin/insserv", 
+                "uname": r"uname(\$|\.|$)"}
+            self.regex_process = {
+                "command": ["^sed$", "^sh$", "^chkconfig$", "^systemctl$", "update-rc.d"],
+                "execution file created by xorddos": "/boot/[a-z]{10}", # 重複了?
+                "pipe": r"^pipe(\$|\.|$)",}
+            self.regex_net = {
+                "ip":r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", 
+                "port": [r"[a-zA-z]:\d{1,5}$", r"port \d{1,5}$"], # 空白需要額外寫 rule 辨識
+                "ip + port": r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$"}
+            self.regex_mem = {"Memory Address": "0x[0-9a-zA-Z]{8}"}
+            self.regex_ID = {"UID": r"UID(\$|\.|$)", "GID": r"GID(\$|\.|$)"}
+            self.regex_permission = {"permission": r"permission(\s|:)[0-9]{1,4}"}
+            self.all_regex_dict = {**self.regex_file,  **self.regex_process, **self.regex_net, **self.regex_mem, **self.regex_ID, **self.regex_permission}
+        # 整理 regex rules       
+        self.all_regex_list = []
+        for v in self.all_regex_dict.values():
+            if isinstance(v,list):
+                for i in v:
+                    self.all_regex_list.append(i)
+            else:
+                self.all_regex_list.append(v)
+        pass
+
+    def get_all_regex(self) -> list: # should turn into classmethod
+        return self.all_regex_list
+
+    def get_used_regex(self) -> set:
+        return self.used_regex_set
+
+    def find_spacial_token(self, sentence:str) -> list[RegexMatchResult]:
+        if not isinstance(sentence, str):
+            try:
+                sentence = str(sentence)
+            except:
+                raise TypeError("RegexMaster: Sentence cannot convert into string.")
+        result_list = []
+        for word in sentence.split():
+            for r in self.all_regex_list:
+                # print('r', r)
+                if re.search(r, word, re.IGNORECASE):
+                    # print("Match :", word, 'with regex', r)
+                    result_list.append(RegexMatchResult(word, match_regex=r)) # 暫時忽略 type
+                    self.used_regex_set.add(r)
+        if len(result_list):
+            return result_list     
+        return None
+
+    def get_regex_type(self): # should turn into classmethod
+        pass
+
+    @classmethod
+    def find_spacial_token_with_regex(cls, regex: str|list, sentence:str) -> list[RegexMatchResult]:
+        ''' Return a list of RegexMatchResult which contains matched 'word' if regex find it. If not found return None'''
+        if not isinstance(sentence, str):
+            try:
+                sentence = str(sentence)
+            except:
+                raise TypeError("RegexMaster: Sentence cannot convert into string.")
+        result_list = []
+        for word in sentence.split():
+            if isinstance(regex, str):
+                isMatch = re.search(regex, word, re.IGNORECASE)
+                if isMatch:
+                    # print("Match :", word, 'with regex', regex)
+                    result_list.append(RegexMatchResult(word, match_regex=regex))
+            elif isinstance(regex, list):
+                for r in regex:
+                    if re.search(r, word, re.IGNORECASE):
+                        # print("Match :", word, 'with regex', r)
+                        result_list.append(RegexMatchResult(word, match_regex=r)) # 暫時忽略 type
+            else:
+                raise TypeError("regex must be str or list.")
+        if len(result_list):
+            return result_list     
+        return None
+
+# test case
+if __name__ == '__main__':
+    sentence = "How does the sed command edit a file? It creates a tmp file /etc/sedQhw17q to store your input first. Try binary 111 find 5.5.5.5:33"
+    regex = "/etc/sed.*"
+    regex_list = [r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+", r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", r":\d+"]
+    result = RegexMaster.find_spacial_token_with_regex(regex, sentence)
+    print(result)
+
+    '''
+    [<RegexMatchResult word=sed, type=None, match_regex=^sed$>,
+    <RegexMatchResult word=/etc/sedQhw17q, type=None, match_regex=/etc/sed.*>,
+    <RegexMatchResult word=5.5.5.5:33., type=None, match_regex=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+>,
+    <RegexMatchResult word=5.5.5.5:33., type=None, match_regex=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}>]
+    '''
